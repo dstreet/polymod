1514d3e6fd33951cb1b9dc9539481b07f2aac8a9
David Street <david@exclamationlabs.com>
2017-07-06 21:53:57 -0400

    Add QueryResult to default export

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = {
 	Model: require('./src/model'),
 	Document: require('./src/document'),
-	Query: require('./src/query'),
+	Query: require('./src/query').Query,
+	QueryResult: require('./src/query').QueryResult,
 	MemStore: require('./src/mem-store'),
 	MemSource: require('./src/mem-source')
 }
